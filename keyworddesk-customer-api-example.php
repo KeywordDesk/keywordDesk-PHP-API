2d89c4943e8f8c76dbea5a4599ebc8022306be72
Peter Schumacher <p.schumacher@encircle360.com>
2014-06-02 20:49:37 +0200

    example extended

--- keyworddesk-customer-api-example.php
+++ keyworddesk-customer-api-example.php
@@ -31,15 +31,34 @@ $AllCount = $keyworddesk->getKeywordCount(KeyworddeskApi::$COUNT_TYPE_ALL);
 $keywordFilter = new KeywordFilter();
 $keywordFilter->setKeyword("handy");
 $keywordFilter->setOpKeyword(KeyworddeskApi::$FILTER_ILIKE);
+
+// searchvolume
 $keywordFilter->setSearchVolume("1000");
 $keywordFilter->setSearchVolumeTo("5000");
+
+// suggested Bid
 $keywordFilter->setSuggestedBidTo("1.4");
 $keywordFilter->setSuggestedBid("0.0");
+
+// termncount
 $keywordFilter->setTermCount("1");
 $keywordFilter->setTermCountTo("6");
+
+// Resultcount
+$keywordFilter->setGoogleResultCount("0");
+$keywordFilter->setGoogleResultCountTo("200000");
+
+// Intitlecount
+$keywordFilter->setGoogleInTitleCount("0");
+$keywordFilter->setGoogleInTitleCountTo("1000000");
+
 $keywordFilter->setOpSuggestedBid(KeyworddeskApi::$FILTER_NUMERIC_BETWEEN);
 $keywordFilter->setOpSearchVolume(KeyworddeskApi::$FILTER_NUMERIC_BETWEEN);
+
 $keywordFilter->setOpTermCount(KeyworddeskApi::$FILTER_NUMERIC_BETWEEN);
+$keywordFilter->setOpGoogleResultCount(KeyworddeskApi::$FILTER_NUMERIC_BETWEEN);
+$keywordFilter->setOpGoogleInTitleCount(KeyworddeskApi::$FILTER_NUMERIC_BETWEEN);
+
 $keywordFilter->setMax(100);
 $keywordFilter->setFieldsToGet(array('suggestedBid','googleInTitleCount','googleResultCount','searchVolume'));
 
